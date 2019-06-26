f47b6676996f812c2f144a349b2562be23cbbda2
fulara <fulara@vrdevvdi006.iggroup.local>
2019-06-26 14:37:01 +0100

    FINP3-2007 boost is now only added by conna if cppstd is 11 or 14.

--- conanfile.py
+++ conanfile.py
@@ -10,20 +10,26 @@ class WiseEnum(ConanFile):
     version = "0.0.1"
     description = "A reflective enum implementation for C++"
     settings = "os", "compiler", "build_type", "arch", "cppstd"
-    default_options = "boost:shared=True"
     generators = "cmake"
-    requires = (
-        'boost/1.68.0@conan/stable'
-    )
     build_requires = "gtest/1.8.1@bincrafters/stable"
     exports_sources = "CMakeLists.txt", "*h", "test*"
 
+    def is_cpp_11_or_14(self):
+        return  "11" in self.settings.cppstd or "14" in self.settings.cppstd
+
+    def configure(self):
+        if self.isCpp14Or11():
+            self.requires("boost/1.68.0@conan/stable")
+        self.default_options.append("boost:shared=True")
+
     def build(self):
         cmake = CMake(self)
+        if self.isCpp14Or11():
+            cmake.definitions["WISE_ENUM_USE_BOOST"] = "ON"
         cmake.configure()
         cmake.build()
         if tools.get_env("CONAN_RUN_TESTS", False):
             cmake.test()
 
     def package(self):
-        self.copy("*.h", dst="include", src="")
+        self.copy("*.h", dst="include/wise_enum", src="")
